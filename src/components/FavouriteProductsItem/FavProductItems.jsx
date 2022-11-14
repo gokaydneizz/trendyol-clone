518d126453b0b3b3e98ef2447b3137c18d92b964
Gokay <77916984+quArex-rQer@users.noreply.github.com>
2022-11-14 19:06:31 +0300

    Delete src/components/FavouriteProductsItem directory

--- src/components/FavouriteProductsItem/FavProductItems.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./FavProductItems.module.css";
-
-const FavProductItems = ({ product }) => {
-  return <main className={styles.container}>FavProductsItem</main>;
-};
-
-export default FavProductItems;
